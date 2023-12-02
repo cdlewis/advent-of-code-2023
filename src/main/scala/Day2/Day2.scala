e6620d642d462c0873209996a826be8ac6fbe22e
Chris Lewis <me@chrislewis.au>
2023-12-02 09:38:07 -0500

    day2

---
+++ src/main/scala/Day2/Day2.scala
@@ -0,0 +1,29 @@
+package Day2
+
+import scala.io.Source
+import scala.util.Using
+import AOC.Reader
+
+case class Colours(red: Int, green: Int, blue: Int)
+
+object Day2 {
+    def solve(): Int = {
+        val re = """(([0-9]+) (red|green|blue))""".r
+        val games = Reader
+            .Read("day2_test")
+            .split("\n")
+            .map(re.findAllIn) // find all pairs
+            .map(_.map { token => // parse the pairs into (int, colour) tuples
+                val pair = token.split(" ")
+                (pair(0).toInt, pair(1))
+            })
+            .map(_.toSeq)
+
+        games
+            .map(
+                _.groupBy(_._2) // group each pair by its colour
+                    .map(_._2.map(_._1)) // convert (Count, Colour) to just Count
+                    .map(_.max).product) // find the max of each colour group and multiply them
+            .sum
+    }
+}
---
+++ src/test/scala/Day2/Day2Tests.scala
@@ -0,0 +1,11 @@
+package Day2
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day2Tests extends AnyFunSuite {
+
+    test("Day2 runs as expected") {
+        assert(Day2.solve() == 77021)
+    }
+
+}
