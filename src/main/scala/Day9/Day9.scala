9e326f0fc592e419095177f8602f40b143f073e3
Chris Lewis <me@chrislewis.au>
2023-12-09 13:12:57 -0500

    day 9

---
+++ src/main/scala/Day9/Day9.scala
@@ -0,0 +1,31 @@
+package Day9
+
+import AOC.Reader
+import AOC.ArrayHelpers
+
+object Day9 {
+
+    @main
+    def solve(): Int = {
+        val sensorReadings = Reader.Read("day9_test").linesIterator.map(_.split(' ').map(_.toInt))
+
+        val extrapolatedSensorValues = sensorReadings.map { row =>
+            // continually find the difference between adjacent elements until the role is all zeroes
+            val adjacentNumberDifferences = Iterator
+                .iterate(row) { _
+                    .sliding(2)
+                    .toArray
+                    .map(_.reduce((x, y) => y - x))
+                }
+                .takeWhile(_.exists(_ != 0))
+
+            // work backwards up the arrays of differences calculating the extrapolated head element as we go
+            adjacentNumberDifferences.toArray
+            .reverseIterator
+            .foldLeft(0) { case (result, currentRow) => currentRow.head - result }
+        }
+
+        // Sum the extrapolated readings to find our solution
+        extrapolatedSensorValues.sum
+    }
+}
---
+++ src/test/scala/Day9Tests/Day9Tests.scala
@@ -0,0 +1,12 @@
+package Day9
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class Day9Tests extends AnyFunSuite {
+
+    test("Day9 runs as expected") {
+        assert(Day9.solve() == 1041)
+    }
+
+}
+
